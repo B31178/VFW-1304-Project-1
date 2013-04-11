47586abab0cb685ce2de3f14f2144e17553e1819
B31178 <travisp@fullsail.edu>
2013-04-11 11:43:04 -0400

    Added main.js & style.css.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-<!--
+/*
 Travis Pochintesta
 VFW 1304
 Project 1
 main.js
--->
+*/
+
+/*
+var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var phoneNumber = document.getElementById("phoneNumber").value;
+var streetAddress = document.getElementById("streetAddress").value;
+var emailAddress = document.getElementById("emailAddress").value;
+
+document.addEventListener("event", function);
+
+document.setAttribute("attribute name", "attribute value");
+
+document.removeAttribute("attribute name", attribute value");
+
+.innerHtml;
+
+.createElement("string");
+
+.appendChild("string");
+
+*/
